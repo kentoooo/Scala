b2b7fdef8dd343d97d240cb2251d335d03bf7cf1
Kento Okamura <kentooooo.1230@gmail.com>
2017-11-03 23:44:29 +0900

    15.Scala のトレイト

---
+++ BasicCourse/08/scala-grammar/src/main/scala-2.11/Trait.scala
@@ -0,0 +1,20 @@
+trait TraitA {
+  def greet(): Unit = println("Hello!")
+}
+
+trait TraitB extends TraitA {
+  override def greet(): Unit = {
+    super.greet()
+    println("My name is Terebi-chan.")
+  }
+}
+
+trait TraitC extends TraitA {
+  override def greet(): Unit = {
+    super.greet()
+    println("I like niconico")
+  }
+}
+
+class Class1 extends TraitB with TraitC
+class Class2 extends TraitC with TraitB
